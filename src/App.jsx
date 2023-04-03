551c07c9c519f955088f304a53a0578a77152124
luminus24 <luminus128@gmail.com>
2023-04-03 17:58:40 -0500

    Proyecto Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,6 @@ function App() {
   const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})
 
-  useEffect(() => {
-    const obtenerLS = () => {
-      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []
-      setPacientes(pacientesLS)
-    }
-    obtenerLS()
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
-  }, [pacientes])
-
-
   const eliminarPaciente = id => {
     const PacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
     setPacientes(PacientesActualizados)
